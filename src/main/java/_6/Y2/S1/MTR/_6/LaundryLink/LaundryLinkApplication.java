e42871914f9d1129a3ec0200ae588502760a7172
IT25103615 <IT25103615@my.sliit.lk>
2026-08-13 21:32:13 +0530

    Initialize the project

---
+++ src/main/java/_6/Y2/S1/MTR/_6/LaundryLink/LaundryLinkApplication.java
@@ -0,0 +1,13 @@
+package _6.Y2.S1.MTR._6.LaundryLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/_6/Y2/S1/MTR/_6/LaundryLink/LaundryLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package _6.Y2.S1.MTR._6.LaundryLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
