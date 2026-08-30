8d8e665d620290f98397f0b366309bf602003f38
IT25103615 <IT25103615@my.sliit.lk>
2026-08-30 12:24:20 +0530

    Created Log Entity, Repository, Service, and Controller.

---
+++ src/main/java/_6/Y2/S1/MTR/_6/LaundryLink/logs/LogService.java
@@ -0,0 +1,31 @@
+package _6.Y2.S1.MTR._6.LaundryLink.logs;
+
+import _6.Y2.S1.MTR._6.LaundryLink.status.StatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+public class LogService {
+    private final LogRepository logRepository;
+    private final StatusService statusService;
+
+    public List<Log> getAllLogs() {
+        return logRepository.findAll();
+    }
+
+    public Optional<Log> getLogById(Integer id) {
+        return logRepository.findById(id);
+    }
+
+    public List<Log> getLogsByOrder(Integer orderID) {
+        return logRepository.findByOrderID(orderID);
+    }
+
+    public Log logChange(Log log) {
+        return logRepository.save(log);
+    }
+    //TODO: change logChange to handle the change rather than depending on other classes to pass in a new log entity
+}
